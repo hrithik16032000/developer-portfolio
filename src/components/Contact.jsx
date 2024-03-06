f6a7b4ece35573687f08d60f2697c0022d5d048e
hrithik16032000 <hrithikgavankar8582@gmail.com>
2024-03-07 00:01:21 +0530

    setting up env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,6 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-const { VITE_APP_EMAILJS_SERVICE_ID, VITE_APP_EMAILJS_TEMPLATE_ID, VITE_APP_EMAILJS_PUBLIC_KEY } = import.meta.env;
-
 
 const Contact = () => {
   const formRef = useRef();
